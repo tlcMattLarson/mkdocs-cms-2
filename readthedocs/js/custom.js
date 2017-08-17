d4bdee9fb4a0444e29c34132ad2a350ff42aecec
tlcMattLarson <30934703+tlcMattLarson@users.noreply.github.com>
2017-08-17 17:21:08 -0500

    nav

--- readthedocs/js/custom.js
+++ readthedocs/js/custom.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+  $("a.current").siblings(".subnav").css("display","inherit");
+
   $("nav .wy-menu li").click(function(){
     $(this).children(".subnav").css("display","inherit");
     $(this).siblings().children(".subnav").css("display","none");
