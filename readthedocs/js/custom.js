7300373020fe47ac499e352da38072c34e7584bd
tlcMattLarson <30934703+tlcMattLarson@users.noreply.github.com>
2017-08-18 12:49:45 -0500

    changes

--- readthedocs/js/custom.js
+++ readthedocs/js/custom.js
@@ -2,7 +2,14 @@ $(document).ready(function(){
   $(".current").siblings(".subnav").css("display","inherit");
 
   $("nav .wy-menu li").click(function(){
+    var navID = location.href.substr(location.href.lastIndexOf('/') + 1);
+    $(.activeNavItem).removeClass('active');
+    $("nav a[href^='"+ navID + "']").addClass('activeNavItem');
+
     $(this).children(".subnav").css("display","inherit");
     $(this).siblings().children(".subnav").css("display","none");
   })
+
+  var navID = location.href.substr(location.href.lastIndexOf('/') + 1);
+  $("nav a[href^='"+ navID + "']").addClass('activeNavItem');
 })
