dd79f2a68744107a1e51e1de233ee2feafa0b698
tlcMattLarson <30934703+tlcMattLarson@users.noreply.github.com>
2017-08-18 14:00:38 -0500

    js fix

--- readthedocs/js/custom.js
+++ readthedocs/js/custom.js
@@ -5,10 +5,15 @@ $(document).ready(function(){
     /*var navID = location.href.substr(location.href.lastIndexOf('/') + 1);
     $(".activeNavItem").removeClass('activeNavItem');
     $("nav a[href^='"+ navID + "']").addClass('activeNavItem');*/
-    $(".activeNavItem").removeClass('activeNavItem');
-    $(this).addClass('activeNavItem');
+
     $(this).children(".subnav").css("display","inherit");
     $(this).siblings().children(".subnav").css("display","none");
+  })
+
+  $("nav .wy-menu li a").click(function(){
+    $(".current").removeClass('current');
+    $(".activeNavItem").removeClass('activeNavItem');
+    $(this).addClass('activeNavItem');
   })
 
   /*var navID = location.href.substr(location.href.lastIndexOf('/') + 1);
